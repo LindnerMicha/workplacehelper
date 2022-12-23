5b35dd053ca79354fa9d8ba589730bd223219441
MethMicha <michael.k.lindner@t-online.de>
2022-12-23 02:53:48 +0100

    textcoplor / read / rite anfang

--- notitz.py
+++ notitz.py
@@ -64,13 +64,13 @@ button(mlfb_analog[12][0], 0, 870, 300, 30, (155,150,100), (0,100,255), sys_font
 if option == "Backgroundcolor":
     button("+", 400, 100, 30, 30, (155,150,100), (0,100,255), sys_font22)
     draw_text("Red[r]", sys_font30, (0,0,0), screen, 480 , 100)
-    draw_text(f"{topbar_r}", sys_font30, (0,0,0), screen, 650 , 100)
+    draw_text(f"{text_r}", sys_font30, (0,0,0), screen, 650 , 100)
     button("set 0", 770, 100, 60, 30, (155,150,100), (0,100,255), sys_font22)
     draw_text("Green[g]", sys_font30, (0,0,0), screen, 480 , 150)
-    draw_text(f"{topbar_g}", sys_font30, (0,0,0), screen, 650 , 150)
+    draw_text(f"{text_g}", sys_font30, (0,0,0), screen, 650 , 150)
     button("set 0", 770, 150, 60, 30, (155,150,100), (0,100,255), sys_font22)
     draw_text("Blue[b]", sys_font30, (0,0,0), screen, 480 , 200)
-    draw_text(f"{topbar_b}", sys_font30, (0,0,0), screen, 650 , 200)
+    draw_text(f"{text_g}", sys_font30, (0,0,0), screen, 650 , 200)
     button("set 0", 770, 200, 60, 30, (155,150,100), (0,100,255), sys_font22)
     button("-", 900, 100, 30, 30, (155,150,100), (0,100,255), sys_font22)
 
@@ -79,93 +79,93 @@ if option == "Backgroundcolor":
 
 if but_txt == "+" and but_y == 100:
     option_setting = "+"
-    if topbar_r < 255:
-        topbar_r += 1
+    if text_r < 255:
+        text_r += 1
     else:
-        topbar_r = 0
+        text_r = 0
 elif but_txt == "-" and but_y == 100:
     option_setting = "-"
-    if topbar_r > 0:
-        topbar_r -= 1
+    if text_r > 0:
+        text_r -= 1
     else:
-        topbar_r = 255
+        text_r = 255
 if but_txt == "+" and but_y == 150:
     option_setting = "+"
-    if topbar_g < 255:
-        topbar_g += 1
+    if text_g < 255:
+        text_g += 1
     else:
-        topbar_g = 0
+        text_g = 0
 elif but_txt == "-" and but_y == 150:
     option_setting = "-"
-    if topbar_g > 0:
-        topbar_g -= 1
+    if text_g > 0:
+        text_g -= 1
     else:
-        topbar_g = 255
+        text_g = 255
 if but_txt == "+" and but_y == 200:
     option_setting = "+"
-    if topbar_b < 255:
-        topbar_b += 1
+    if text_g < 255:
+        text_g += 1
     else:
-        topbar_b = 0
+        text_g = 0
 elif but_txt == "-" and but_y == 200:
     option_setting = "-"
-    if topbar_b > 0:
-        topbar_b -= 1
+    if text_g > 0:
+        text_g -= 1
     else:
-        topbar_b = 255
+        text_g = 255
 
 if but_txt == "set 0" and but_y == 100:
-    topbar_r = 0
+    text_r = 0
 elif but_txt == "set 255" and but_y == 100:
-    topbar_r = 255
+    text_r = 255
 if but_txt == "set 0" and but_y == 150:
-    topbar_g = 0
+    text_g = 0
 elif but_txt == "set 255" and but_y == 150:
-    topbar_g = 255
+    text_g = 255
 if but_txt == "set 0" and but_y == 200:
-    topbar_b = 0
+    text_g = 0
 elif but_txt == "set 255" and but_y == 200:
-    topbar_b = 255
+    text_g = 255
 
 
 
     if option == "Buttoncolor":
-        button_setting("+", 400, 100, 30, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
+        button_setting("+", 400, 100, 30, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
         draw_text("Red[r]", sys_font30, (0,0,0), screen, 480 , 100)
-        draw_text(f"{topbar_r}", sys_font30, (0,0,0), screen, 650 , 100)
-        button_setting("set 0", 770, 100, 60, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
-        button_setting("set 255", 870, 100, 60, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
-        button_setting("-", 1000, 100, 30, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
+        draw_text(f"{text_r}", sys_font30, (0,0,0), screen, 650 , 100)
+        button_setting("set 0", 770, 100, 60, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
+        button_setting("set 255", 870, 100, 60, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
+        button_setting("-", 1000, 100, 30, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
 
-        button_setting("+", 400, 150, 30, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
+        button_setting("+", 400, 150, 30, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
         draw_text("Green[g]", sys_font30, (0,0,0), screen, 480 , 150)
-        draw_text(f"{topbar_g}", sys_font30, (0,0,0), screen, 650 , 150)
-        button_setting("set 0", 770, 150, 60, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
-        button_setting("set 255", 870, 150, 60, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
-        button_setting("-", 1000, 150, 30, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
+        draw_text(f"{text_g}", sys_font30, (0,0,0), screen, 650 , 150)
+        button_setting("set 0", 770, 150, 60, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
+        button_setting("set 255", 870, 150, 60, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
+        button_setting("-", 1000, 150, 30, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
 
-        button_setting("+", 400, 200, 30, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
+        button_setting("+", 400, 200, 30, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
         draw_text("Blue[b]", sys_font30, (0,0,0), screen, 480 , 200)
-        draw_text(f"{topbar_b}", sys_font30, (0,0,0), screen, 650 , 200)
-        button_setting("set 0", 770, 200, 60, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
-        button_setting("set 255", 870, 200, 60, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
-        button_setting("-", 1000, 200, 30, 30, (topbar_r,topbar_g,topbar_b), (0,100,255), sys_font22)
+        draw_text(f"{text_g}", sys_font30, (0,0,0), screen, 650 , 200)
+        button_setting("set 0", 770, 200, 60, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
+        button_setting("set 255", 870, 200, 60, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
+        button_setting("-", 1000, 200, 30, 30, (text_r,text_g,text_g), (0,100,255), sys_font22)
 
         if pressed[pygame.K_r]:
-            if topbar_r >= 255:
-                topbar_r = 0
+            if text_r >= 255:
+                text_r = 0
             else:
-                topbar_r +=1
+                text_r +=1
         if pressed[pygame.K_g]:
-            if topbar_g >= 255:
-                topbar_g = 0
+            if text_g >= 255:
+                text_g = 0
             else:
-                topbar_g +=1
+                text_g +=1
         if pressed[pygame.K_b]:
-            if topbar_b >= 255:
-                topbar_b = 0
+            if text_g >= 255:
+                text_g = 0
             else:
-                topbar_b +=1
+                text_g +=1
 
 
 
@@ -177,53 +177,53 @@ elif but_txt == "set 255" and but_y == 200:
 
                 if but_txt == "+" and but_y == 100:
                     option_setting = "+"
-                    if topbar_r < 255:
-                        topbar_r += 1
+                    if text_r < 255:
+                        text_r += 1
                     else:
-                        topbar_r = 0
+                        text_r = 0
                 elif but_txt == "-" and but_y == 100:
                     option_setting = "-"
-                    if topbar_r > 0:
-                        topbar_r -= 1
+                    if text_r > 0:
+                        text_r -= 1
                     else:
-                        topbar_r = 255
+                        text_r = 255
                 if but_txt == "+" and but_y == 150:
                     option_setting = "+"
-                    if topbar_g < 255:
-                        topbar_g += 1
+                    if text_g < 255:
+                        text_g += 1
                     else:
-                        topbar_g = 0
+                        text_g = 0
                 elif but_txt == "-" and but_y == 150:
                     option_setting = "-"
-                    if topbar_g > 0:
-                        topbar_g -= 1
+                    if text_g > 0:
+                        text_g -= 1
                     else:
-                        topbar_g = 255
+                        text_g = 255
                 if but_txt == "+" and but_y == 200:
                     option_setting = "+"
-                    if topbar_b < 255:
-                        topbar_b += 1
+                    if text_g < 255:
+                        text_g += 1
                     else:
-                        topbar_b = 0
+                        text_g = 0
                 elif but_txt == "-" and but_y == 200:
                     option_setting = "-"
-                    if topbar_b > 0:
-                        topbar_b -= 1
+                    if text_g > 0:
+                        text_g -= 1
                     else:
-                        topbar_b = 255
+                        text_g = 255
 
                 if but_txt == "set 0" and but_y == 100:
-                    topbar_r = 0
+                    text_r = 0
                 elif but_txt == "set 255" and but_y == 100:
-                    topbar_r = 255
+                    text_r = 255
                 if but_txt == "set 0" and but_y == 150:
-                    topbar_g = 0
+                    text_g = 0
                 elif but_txt == "set 255" and but_y == 150:
-                    topbar_g = 255
+                    text_g = 255
                 if but_txt == "set 0" and but_y == 200:
-                    topbar_b = 0
+                    text_g = 0
                 elif but_txt == "set 255" and but_y == 200:
-                    topbar_b = 255
+                    text_g = 255
 
 
 
@@ -232,37 +232,95 @@ elif but_txt == "set 255" and but_y == 200:
 
             button_setting("+", 400, 100, 30, 30, (155, 150, 100), (0,100,255), sys_font22)
             draw_text("Red[r]", sys_font30, (0,0,0), screen, 480 , 100)
-            draw_text(f"{topbar_r}", sys_font30, (0,0,0), screen, 650 , 100)
+            draw_text(f"{text_r}", sys_font30, (0,0,0), screen, 650 , 100)
             button_setting("set 0", 770, 100, 60, 30, (155, 150, 100), (0,100,255), sys_font22)
             button_setting("set 255", 870, 100, 60, 30, (155, 150, 100), (0,100,255), sys_font22)
             button_setting("-", 1000, 100, 30, 30, (155, 150, 100), (0,100,255), sys_font22)
 
             button_setting("+", 400, 150, 30, 30, (155, 150, 100), (0,100,255), sys_font22)
             draw_text("Green[g]", sys_font30, (0,0,0), screen, 480 , 150)
-            draw_text(f"{topbar_g}", sys_font30, (0,0,0), screen, 650 , 150)
+            draw_text(f"{text_g}", sys_font30, (0,0,0), screen, 650 , 150)
             button_setting("set 0", 770, 150, 60, 30, (155, 150, 100), (0,100,255), sys_font22)
             button_setting("set 255", 870, 150, 60, 30, (155, 150, 100), (0,100,255), sys_font22)
             button_setting("-", 1000, 150, 30, 30, (155, 150, 100), (0,100,255), sys_font22)
 
             button_setting("+", 400, 200, 30, 30, (155, 150, 100), (0,100,255), sys_font22)
             draw_text("Blue[b]", sys_font30, (0,0,0), screen, 480 , 200)
-            draw_text(f"{topbar_b}", sys_font30, (0,0,0), screen, 650 , 200)
+            draw_text(f"{text_g}", sys_font30, (0,0,0), screen, 650 , 200)
             button_setting("set 0", 770, 200, 60, 30, (155, 150, 100), (0,100,255), sys_font22)
             button_setting("set 255", 870, 200, 60, 30, (155, 150, 100), (0,100,255), sys_font22)
             button_setting("-", 1000, 200, 30, 30, (155, 150, 100), (0,100,255), sys_font22)
 
             if pressed[pygame.K_r]:
-                if topbar_r >= 255:
-                    topbar_r = 0
+                if text_r >= 255:
+                    text_r = 0
                 else:
-                    topbar_r +=1
+                    text_r +=1
             if pressed[pygame.K_g]:
-                if topbar_g >= 255:
-                    topbar_g = 0
+                if text_g >= 255:
+                    text_g = 0
                 else:
-                    topbar_g +=1
+                    text_g +=1
             if pressed[pygame.K_b]:
-                if topbar_b >= 255:
-                    topbar_b = 0
+                if text_g >= 255:
+                    text_g = 0
                 else:
-                    topbar_b +=1
+                    text_g +=1
+
+
+
+
+
+
+
+
+            elif option == "Textcolor":
+                if but_txt == "+" and but_y == 100:
+                    option_setting = "+"
+                    if text_r < 255:
+                        text_r += 1
+                    else:
+                        text_r = 0
+                elif but_txt == "-" and but_y == 100:
+                    option_setting = "-"
+                    if text_r > 0:
+                        text_r -= 1
+                    else:
+                        text_r = 255
+                if but_txt == "+" and but_y == 150:
+                    option_setting = "+"
+                    if text_g < 255:
+                        text_g += 1
+                    else:
+                        text_g = 0
+                elif but_txt == "-" and but_y == 150:
+                    option_setting = "-"
+                    if text_g > 0:
+                        text_g -= 1
+                    else:
+                        text_g = 255
+                if but_txt == "+" and but_y == 200:
+                    option_setting = "+"
+                    if text_b < 255:
+                        text_b += 1
+                    else:
+                        text_b = 0
+                elif but_txt == "-" and but_y == 200:
+                    option_setting = "-"
+                    if text_b > 0:
+                        text_b -= 1
+                    else:
+                        text_b = 255
+
+                if but_txt == "set 0" and but_y == 100:
+                    text_r = 0
+                elif but_txt == "set 255" and but_y == 100:
+                    text_r = 255
+                if but_txt == "set 0" and but_y == 150:
+                    text_g = 0
+                elif but_txt == "set 255" and but_y == 150:
+                    text_g = 255
+                if but_txt == "set 0" and but_y == 200:
+                    text_b = 0
+                elif but_txt == "set 255" and but_y == 200:
+                    text_b = 255
